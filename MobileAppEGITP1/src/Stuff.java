31c19aec7e27ce85877a51f86a3f639df5868972
ilongoria6946 <ilongoria6946@001LAB102010551.neisd.ad>
2015-01-08 09:17:46 -0600

    Inessa

--- MobileAppEGITP1/src/Stuff.java
+++ MobileAppEGITP1/src/Stuff.java
@@ -4,7 +4,7 @@ public class Stuff {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	
-		
+		inessa();
 		poore();
 		
 	}
@@ -14,7 +14,9 @@ public class Stuff {
 		System.out.println("Mr. Poore");
 	}
 	
-	
-	
+	public static void inessa()
+	{
+		System.out.println("Hi");
+	}
 }
 

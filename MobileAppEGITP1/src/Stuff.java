48bd75834c0ef6ec6bb4da3169a97afa3e446bc9
ilongoria6946 <ilongoria6946@001LAB102010551.neisd.ad>
2015-01-09 09:24:10 -0600

    il

--- MobileAppEGITP1/src/Stuff.java
+++ MobileAppEGITP1/src/Stuff.java
@@ -14,16 +14,15 @@ public class Stuff {
 		System.out.println("Mr. Poore");
 	}
 	
-<<<<<<< HEAD
 	public static void inessa()
 	{
 		System.out.println("Hi");
 	}
-}
-=======
+
+
+
 	public static void norwood()
 	{
 		System.out.println("Collin Norwood");
 	}
->>>>>>> branch 'master' of https://github.com/LeeSTEM1415MobAppDes1/MADP1-1415.git
-
+}
